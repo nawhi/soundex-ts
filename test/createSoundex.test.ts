ea5c235fe3b50d03be3acf00c28509b3eb575dea
Phil White <pjd.white@gmail.com>
2020-08-19 16:59:32 +0100

    replace consonants with a number

--- test/createSoundex.test.ts
+++ test/createSoundex.test.ts
@@ -2,6 +2,10 @@ import {expect} from "chai";
 
 function createSoundex(input: string): string {
     const firstLetter: string = input[0];
+
+    if(['b','f','p','v'].includes(firstLetter)) {
+        return '1'
+    }
     return firstLetter
 }
 
@@ -17,5 +21,11 @@ describe('createSoundex', () => {
         expect(createSoundex('pw')).to.eql('p');
         expect(createSoundex('pwwww')).to.eql('p');
         expect(createSoundex('pwwhhhh')).to.eql('p');
+    });
+    it('replaces all consonants with number 1', () => {
+        expect(createSoundex('b')).to.eql('1');
+        expect(createSoundex('f')).to.eql('1');
+        expect(createSoundex('p')).to.eql('1');
+        expect(createSoundex('v')).to.eql('1');
     });
 });
