eeb65ac1a636750cc2084deacae2a6ece81e6de2
Phil White <pjd.white@gmail.com>
2020-08-19 16:57:11 +0100

    remove all occurrences of the h & w

--- test/createSoundex.test.ts
+++ test/createSoundex.test.ts
@@ -1,12 +1,21 @@
 import {expect} from "chai";
 
 function createSoundex(input: string): string {
-    return input
+    const firstLetter: string = input[0];
+    return firstLetter
 }
 
 describe('createSoundex', () => {
     it('returns the first letter', () => {
         const result = createSoundex('p');
         expect(result).to.eql('p');
+    });
+    it('removes all occurrences of h and w except first letter', () => {
+        expect(createSoundex('h')).to.eql('h');
+        expect(createSoundex('w')).to.eql('w');
+        expect(createSoundex('ph')).to.eql('p');
+        expect(createSoundex('pw')).to.eql('p');
+        expect(createSoundex('pwwww')).to.eql('p');
+        expect(createSoundex('pwwhhhh')).to.eql('p');
     });
 });
